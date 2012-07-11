a8d47f5c9ffc06efded007c74e7511654b81bbab
Justin Hamilton <justin.hamilton@prairieventures.net>
2012-07-11 18:48:12 -0500

    Making sure post01 matches the blog exactly.

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+--file: Main.hs
 module Main where
 
 import Prelude hiding (Either(..))
@@ -6,20 +7,22 @@ import System.IO
 
 type Coord = (Int, Int)
 
-data Direction = Up
-               | Down
-               | Left
-               | Right
-
-data Input = Dir Direction 
-           | Exit
-
 data World = World { wHero :: Coord }
 
+data Input = Up
+           | Down
+           | Left
+           | Right
+           | Exit
+           deriving (Eq)
 
--- operator to add 2 coordinates together
-(|+|) :: Coord -> Coord -> Coord
-(|+|) (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+main = do
+  hSetEcho stdin False
+  hSetBuffering stdin  NoBuffering
+  hSetBuffering stdout NoBuffering
+  hideCursor
+  setTitle "Thieflike"
+  gameLoop $ World (0, 0)
 
 
 drawHero (heroX, heroY) = do
@@ -29,37 +32,50 @@ drawHero (heroX, heroY) = do
          , SetColor Foreground Vivid Blue ]
   putStr "@"
 
-
 -- receive a character and return our Input data structure,
 -- recursing on invalid input
 getInput = do
   char <- getChar
   case char of
     'q' -> return Exit
-    'w' -> return (Dir Up)
-    's' -> return (Dir Down)
-    'a' -> return (Dir Left)
-    'd' -> return (Dir Right)
-    _   -> getInput
+    'w' -> return Up
+    's' -> return Down
+    'a' -> return Left
+    'd' -> return Right
+    _ -> getInput
 
 
--- translate a direction to a coordinate so it can be added to
--- the hero's coordinate to move the hero around
-dirToCoord Up    = (0, -1)
-dirToCoord Down  = (0,  1)
-dirToCoord Left  = (-1, 0)
-dirToCoord Right = (1,  0)
+-- operator to add 2 coordinates together
+(|+|) :: Coord -> Coord -> Coord
+(|+|) (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
 
--- add the supplied direction to the hero's position, 
--- and set that to be the hero's new position, making 
--- sure to limit it between 0 and 80 in either direction
-handleDir w@(World hero) dir = gameLoop (w { wHero = newCoord })
+dirToCoord d
+  | d == Up    = (0, -1)
+  | d == Down  = (0,  1)
+  | d == Left  = (-1, 0)
+  | d == Right = (1,  0)
+  | otherwise  = (0,  0)
+                 
+
+-- add the supplied direction to the hero's position, and set that
+-- to be the hero's new position, making sure to limit the hero's
+-- position between 0 and 80 in either direction
+handleDir w@(World hero) input = gameLoop (w { wHero = newCoord })
   where newCoord       = (newX, newY)
-        (heroX, heroY) = hero |+| dirToCoord dir
+        (heroX, heroY) = hero |+| (dirToCoord input)
         hConst i       = max 0 (min i 80)
         newX           = hConst heroX
         newY           = hConst heroY
+        
+
+-- update the game loop to add in the goodbye message
+gameLoop world@(World hero) = do
+  drawHero hero
+  input <- getInput
+  case input of
+    Exit -> handleExit
+    _    -> handleDir world input
 
 
 -- when the user wants to exit we give them a thank you
@@ -69,21 +85,3 @@ handleExit = do
   setCursorPosition 0 0
   showCursor
   putStrLn "Thank you for playing!"
-  
-
--- update the game loop to add in the goodbye message
-gameLoop world@(World hero) = do
-  drawHero hero
-  input <- getInput
-  case input of
-    Exit    -> handleExit
-    Dir dir -> handleDir world dir
-
-
-main = do
-  hSetEcho stdin False
-  hSetBuffering stdin  NoBuffering
-  hSetBuffering stdout NoBuffering
-  hideCursor
-  setTitle "Thieflike"
-  gameLoop $ World (0, 0)
